72b37d603ffe2654e3e19fb2344d87b3de52cfba
SALAS DRAI <ismael.montoya@udea.edu.co>
2025-07-31 10:15:38 -0500

    falta solo un error

--- js/newton-binomy.js
+++ js/newton-binomy.js
@@ -2,23 +2,23 @@ const txtA = document.getElementById('txt-a');
 const txtB = document.getElementById('txt-b');
 const txtN = document.getElementById('txt-n');
 const btnDev = document.getElementById('newton-binomy');
+const divResult = document.getElementById('div-result');
 
 btnDev.addEventListener('click', (e) => {
-    let msg = `
+    let binomy = `
         termino 1: + ${txtA.value};
-    <br>
+        <br>
         termino 2: + ${txtB.value}`;
-    divResult.innerHTML = msg;
+    divResult.innerHTML = binomy;
+    let expA = txtN.value
+    let expB = 0
+    let coef = 1
+    binomy += txtA.value + "^" + expA
+    for (let i = 1; i <= txtN.value; i++) {
+        coef = expA * coef / (expB + 1)
+        expA--
+        expB++
+        binomy += "+" + coef + txtA.value + "^" + expA + txtB.value + "^" + expB
+    }
+    divResult.innerHTML = binomy
 })
-function (newtonBinomy[a, b, N]){
-let expA = N
-let expB = 0
-let coef = 1
-let binomy = a + "^" + expA
-for (let i = 1; i <= n; i++) {
-    coef = expA * coef / (expB + 1)
-    expA--
-    expB++
-    binomy += "+" + coef + a + "^" + expA + b + "^" + expB
-}
-}
